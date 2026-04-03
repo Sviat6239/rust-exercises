0fa105ee340e346cd884c4133a297058caf9b8ee
Sviat6239 <sviatoslav6239@gmail.com>
2026-04-03 17:18:02 +0200

    added loop inte the game

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -10,23 +10,30 @@ fn main() {
     //Random number
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    //Infinity loop
+    loop{
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    //User input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faild to read line");
+        //User input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
 
-    //Conver the input number from string into integer value
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-      
+        //Conver the input number from string into integer value
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
 
-    //Comparing the secret number with user input
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        //Comparing the secret number with user input
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
+
 }
