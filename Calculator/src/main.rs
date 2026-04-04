32adb8dee6054f29071390eeac284e1f30e4c458
Sviat6239 <sviatoslav6239@gmail.com>
2026-04-04 14:07:14 +0200

    Enchanced types, now it works

--- Calculator/src/main.rs
+++ Calculator/src/main.rs
@@ -2,53 +2,53 @@ use std::io;
 use std::cmp::Ordering;
 
 
-fn add(num1: i64, num2: i64){
-    let buff: i64 = num1 + num2;
+fn add(num1: f64, num2: f64){
+    let buff: f64 = num1 + num2;
     println!("{}", buff);
 }
 
-fn sub(num1: i64, num2: i64){
-    let buff: i64 = num1 - num2;
+fn sub(num1: f64, num2: f64){
+    let buff: f64 = num1 - num2;
     println!("{}", buff);
 }
 
-fn mul(num1: i64, num2: i64){
-    let buff: i64 = num1 * num2;
+fn mul(num1: f64, num2: f64){
+    let buff: f64 = num1 * num2;
     println!("{}", buff);
 }
 
-fn div(num1: i64, num2: i64) ->  i64{
-    if num2 == 0{
+fn div(num1: f64, num2: f64) ->  f64{
+    if num2 == 0.0 {
         println!("Can't divide by zero!");
-        0
+        0.0
     } else {
-        let buff = num1 / num2;
+        let buff: f64 = num1 / num2;
         println!("{}", buff);
         buff
     }
 }
 
-fn sqr(num: i64){
-    let buff: i64 = num * num;
+fn sqr(num: f64){
+    let buff: f64 = num * num;
     println!("{}", buff);
 }
 
-fn pow(num1: i64, num2: i64){
-    let mut buff: i64 = 1;
-    for _ in 0..num2{
+fn pow(num1: f64, num2: f64){
+    let mut buff: f64 = 1.0;
+    for _ in 0..num2 as usize {
         buff = buff * num1;
     }
 
     println!("{}", buff);
 }
 
-fn root(num: i64) -> i64 {
-    let mut i: i64 = 0;
-    let mut result: i64 = 0;
+fn root(num: f64) -> f64 {
+    let mut i: f64 = 0.0;
+    let mut result: f64 = 0.0;
 
     while i * i <= num {
         result = i;
-        i += 1;
+        i += 0.001;
     }
 
     println!("{}", result);
@@ -57,11 +57,11 @@ fn root(num: i64) -> i64 {
 
 
 fn main() {
-    root(23);
-    add(2, 19);
-    div(4, 53);
-    sub(23, 17);
-    mul(3, 11);
-    pow(23, 11);
-    sqr(14);
+    root(23.0);
+    add(2.0, 19.0);
+    div(4.0, 53.0);
+    sub(23.0, 17.0);
+    mul(3.0, 11.0);
+    pow(23.0, 11.0);
+    sqr(14.074);
 }
